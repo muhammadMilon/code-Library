e4f0f77440c13323b9fb1039deb4647dcf9eb385
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2023-10-18 10:46:39 +0600

    Create practice.cpp

---
+++ Data Structure Academic/stack/practice.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool areBracketsBalanced(const string& expression) {
+    stack<char> stack;
+    
+    for (char ch : expression) {
+        if (ch == '(' || ch == '[' || ch == '{') {
+            stack.push(ch);
+        } else if (ch == ')' || ch == ']' || ch == '}') {
+            if (stack.empty()) {
+                return false; // Unmatched closing bracket
+            }
+            
+            char top = stack.top();
+            stack.pop();
+            
+            if ((ch == ')' && top != '(') ||
+                (ch == ']' && top != '[') ||
+                (ch == '}' && top != '{')) {
+                return false; // Mismatched closing bracket
+            }
+        }
+    }
+    
+    return stack.empty(); // Stack should be empty if all brackets are balanced
+}
+
+int main() {
+    string expression;
+    cout << "Enter an expression with brackets: ";
+    cin >> expression;
+
+    if (areBracketsBalanced(expression)) {
+        cout << "Brackets are balanced." << endl;
+    } else {
+        cout << "Brackets are not balanced." << endl;
+    }
+
+    return 0;
+}
