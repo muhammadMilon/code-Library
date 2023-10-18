8fe4beb0800987b14da5951a0c520549bc403795
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2023-10-18 10:49:38 +0600

    Update and rename practice.cpp to BalancedBracketsMatching.cpp

--- Data Structure Academic/Stack/BalancedBracketsMatching.cpp
+++ Data Structure Academic/Stack/BalancedBracketsMatching.cpp
@@ -1,31 +1,36 @@
-#include <iostream>
-#include <stack>
-#include <string>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-bool areBracketsBalanced(const string& expression) {
+bool areBracketsBalanced(string expression)
+{
     stack<char> stack;
-    
-    for (char ch : expression) {
-        if (ch == '(' || ch == '[' || ch == '{') {
+
+    for (char ch : expression)
+    {
+        if (ch == '(' || ch == '[' || ch == '{')
+        {
             stack.push(ch);
-        } else if (ch == ')' || ch == ']' || ch == '}') {
-            if (stack.empty()) {
+        }
+         else if (ch == ')' || ch == ']' || ch == '}')
+        {
+            if (stack.empty())
+            {
                 return false; // Unmatched closing bracket
             }
-            
+
             char top = stack.top();
             stack.pop();
-            
+
             if ((ch == ')' && top != '(') ||
                 (ch == ']' && top != '[') ||
-                (ch == '}' && top != '{')) {
+                (ch == '}' && top != '{'))
+            {
                 return false; // Mismatched closing bracket
             }
         }
     }
-    
+
     return stack.empty(); // Stack should be empty if all brackets are balanced
 }
 
@@ -34,9 +39,12 @@ int main() {
     cout << "Enter an expression with brackets: ";
     cin >> expression;
 
-    if (areBracketsBalanced(expression)) {
+    if (areBracketsBalanced(expression))
+    {
         cout << "Brackets are balanced." << endl;
-    } else {
+    }
+    else
+    {
         cout << "Brackets are not balanced." << endl;
     }
 
