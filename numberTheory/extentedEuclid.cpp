9fd8ccc829e49c6a4af1035bb63d9176b6e04b64
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2023-10-21 10:56:17 +0600

    Create extentedEuclid.cpp

---
+++ numberTheory/extentedEuclid.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int x,y,d;
+
+void extendedEuclid(int a, int b)
+{
+    if (b == 0)
+    {
+        x = 1;
+        y = 0;
+        d = a;
+        return;
+    }
+    extendedEuclid(b, a % b);
+    int x1 = y;
+    int y1 = x - (a / b) * y;
+    x = x1;
+    y = y1;
+}
+
+int main()
+{
+    int a,b;
+    while(cin>>a>>b)
+    {
+        extendedEuclid(a,b);
+        printf("X= %d Y=%d D=%d\n",x,y,d);
+    }
+}
