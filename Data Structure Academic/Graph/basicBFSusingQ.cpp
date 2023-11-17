d3b567d48097a8a051eac5c4df2332157e3f98bc
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2023-11-17 12:40:44 +0600

    Update basicBFSusingQ.cpp

--- Data Structure Academic/Graph/basicBFSusingQ.cpp
+++ Data Structure Academic/Graph/basicBFSusingQ.cpp
@@ -1,69 +1,57 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-vector<int> adj[1000];
-int vis[1000];
+vector<int> v[100000];
+int vis[10000];
+int dis[10000];
+vector<int> bfsTraversal;
 
-void bfs(int s, int n)
+void bfs(int node)
 {
-    // Initialize the 'vis' array to mark all nodes as unvisited (0).
-    for (int i = 0; i < n; i++)
-    {
-        vis[i] = 0;
-    }
-
-    // Create a queue for BFS and push the starting node 's' into it.
     queue<int> q;
-    q.push(s);
-    vis[s] = 1; // Mark the starting node as visited.
-
+    q.push(node);
+    vis[node] = 1;
+    dis[node] = 1;
     while (!q.empty())
     {
-        int u = q.front();
+        int cur = q.front();
+        bfsTraversal.push_back(cur);
         q.pop();
-
-        // Traverse all adjacent nodes of 'u' and mark them as visited.
-        for (int i = 0; i < adj[u].size(); i++)
+        for (int child : v[cur])
         {
-            int v = adj[u][i];
-            if (vis[v] == 0)
-          {
-                vis[v] = 1;
-                q.push(v);
-          }
+            if (vis[child] == 0)
+            {
+                dis[child] = dis[cur] + 1;
+                vis[child] = 1;
+                q.push(child);
+            }
         }
     }
 }
 
 int main()
 {
-    int n, m; // 'n' is the number of nodes, 'm' is the number of edges.
-    cin >> n >> m;
-
-    // Build the adjacency list by reading 'm' edges.
-    for (int i = 0; i < m; i++)
+    int n, e, x, y;
+    cin >> n >> e;
+    while (e--)
     {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        cin >> x >> y;
+        v[x].push_back(y);
+        v[y].push_back(x);
     }
+    bfs(1);
 
-    int s; // Starting node for BFS.
-    cin >> s;
-
-    bfs(s, n);
-
-    // Print the BFS result.
-    cout << "BFS Result starting from node " << s << ": ";
-    for (int i = 0; i < n; i++)
+    cout << "BFS Traversal: ";
+    for (int node : bfsTraversal)
     {
-        if (vis[i] == 1)
-        {
-            cout << i << " ";
-        }
+        cout << node << " ";
     }
     cout << endl;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        cout << "Distance of node " << i << " from the starting node: " << dis[i] << endl;
+    }
 
     return 0;
 }
