f6016c9e886bd889303911154ab108b2d0dccd28
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2023-11-17 23:33:27 +0600

    Create topologicalSORTusingDFS.cpp

---
+++ Data Structure Academic/Graph/topologicalSORTusingDFS.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> v[10000];
+vector<int> res;
+int vis[1003];
+
+void dfs(int node)
+{
+    vis[node] = 1;
+    for (int i = 0; i < v[node].size(); i++)
+    {
+        int child = v[node][i];
+        if (!vis[child])
+        {
+            dfs(child);
+        }
+    }
+    res.push_back(node);
+}
+
+int main()
+{
+    int n, m;
+    cout << "Vertices: ";
+    cin >> n;
+    cout << "Edge: ";
+    cin >> m;
+    int x, y;
+
+    while (m--)
+    {
+        cin >> x >> y;
+        v[x].push_back(y);
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        if(vis[i]==0)
+        {
+            dfs(i);
+        }
+    }
+
+    cout<<"Topological sort ordering: ";
+    for(int i=res.size()-1;i>=0;i--)
+    {
+        cout<<res[i]<<" ";
+    }
+    return 0;
+}
