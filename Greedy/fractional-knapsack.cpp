2f5174a7b01af17e65c6f4b67bd935bc1315abf5
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2024-02-27 16:11:30 +0600

    Create fractional-knapsack.cpp

---
+++ Greedy/fractional-knapsack.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int v,w;
+};
+
+bool cmp(node a,node b)
+{
+    return (a.v*b.w)>(b.v*a.w);
+}
+
+int f_knapsack(vector<node>&v,int w)
+{
+    sort(v.begin(),v.end(),cmp);
+
+    int ans=0;
+    for(const auto &i:v)
+    {
+        if(w>=i.w)
+        {
+            ans+=i.v;
+            w-=i.w;
+        }
+        else
+        {
+            ans+=(i.v/i.w)*w;
+            break;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n,w;
+    cin>>n>>w;
+    vector<node>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i].v>>v[i].w;
+    }
+    cout<<f_knapsack(v,w)<<endl;
+
+    return 0;
+}
