f1ba0fa44027cdd506980a1094d4c08449e2dafa
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2024-03-14 04:05:38 +0600

    Update Matrix Chain Multiplication.cpp

--- Dynamic_Programming/Matrix Chain Multiplication.cpp
+++ Dynamic_Programming/Matrix Chain Multiplication.cpp
@@ -3,33 +3,43 @@ using namespace std;
 
 int main()
 {
-    int a[]={5,4,6,2,7};
-    int n=sizeof(a)/sizeof(a[0]);
-    int m[n][n]={0};
-    int s[n][n]={0};
-    int j,mini,q;
+    int a[] = {5, 4, 6, 2, 7};
+    int n = sizeof(a) / sizeof(a[0]);
+    int m[n][n] = {0};
+    int s[n][n] = {0};
+    int j, mini, q;
 
-    for(int d=1;d<n-1;d++)
+    for (int d = 1; d < n - 1; d++)
     {
-        for(int i=1;i<n-d;i++)
+        for (int i = 1; i < n - d; i++)
         {
-            j=i+d;
-            mini=INT_MAX;
-            for(int k=i;k<=j-1;k++)
+            j = i + d;
+            mini = INT_MAX;
+            for (int k = i; k <= j - 1; k++)
             {
-                q=m[i][k]+m[k+1][j]+(a[i-1]*a[j]*a[k]);
-
-                if(q<mini)
+                q = m[i][k] + m[k + 1][j] + (a[i - 1] * a[j] * a[k]);
+                if (q < mini)
                 {
-                  mini=q;
-                  s[i][j]=k;
+                    mini = q;
+                    s[i][j] = k;
                 }
             }
-            m[i][j]=mini;
+            m[i][j] = mini;
         }
     }
-    cout<<m[1][n-1]<<endl;
+    cout << m[1][n - 1] << endl;
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            cout << s[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
+
 
 
 /*
