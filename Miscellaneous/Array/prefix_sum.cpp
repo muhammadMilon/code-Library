0e3a120135836ba641a1978a8cec590b9eb86b39
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2024-06-06 23:06:44 +0600

    Create prefix_sum.cpp

---
+++ Miscellaneous/Array/prefix_sum.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 9;
+
+int a[N];
+long long prefix_sum[N];
+int main() {
+  int n; cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  prefix_sum[0] = 0;
+  for (int i = 1; i <= n; i++) {
+    prefix_sum[i] = prefix_sum[i - 1] + a[i];
+  }
+  int q; cin >> q;
+  while (q--) {
+    int l, r; cin >> l >> r;
+    l++; r++;
+    long long sum = 0;
+    sum = prefix_sum[r] - prefix_sum[l - 1];
+    cout << sum << '\n';
+  }
+  return 0;
+}
