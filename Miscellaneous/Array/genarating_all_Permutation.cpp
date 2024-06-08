9d6608bee620a82364432fe02c7f8a79916011fd
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2024-06-08 14:45:33 +0600

    Create genarating_all_Permutation.cpp

---
+++ Miscellaneous/Array/genarating_all_Permutation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a[] = {1, 2, 3};
+  int n = 3;
+  sort(a, a + n);
+  do {
+    for (int i = 0; i < n; i++) {
+      cout << a[i] << " ";
+    }
+    cout << '\n';
+  } while (next_permutation(a, a + n));
+  return 0;
+}
